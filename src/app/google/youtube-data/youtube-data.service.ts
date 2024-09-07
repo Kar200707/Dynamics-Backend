fb41fb6fd8ed43b7daa610ba3e0f5ee7b4758372
Karen <karen@Mac-mini-Karen.local>
2024-09-07 07:49:08 -0700

    cokkie fixes

--- src/app/google/youtube-data/youtube-data.service.ts
+++ src/app/google/youtube-data/youtube-data.service.ts
@@ -42,6 +42,7 @@ export class YoutubeDataService {
         agent: agent,
         requestOptions: {
           headers: {
+            "cookie": cookiesFileContent,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
           }
         }
