9af6567060f57ac21eb3fb1e5ee0211cd496cb2d
Karen <karen@Mac-mini-Karen.local>
2024-09-16 00:35:18 -0700

    buffer size 4mb

--- src/app/google/youtube-data/youtube-data.service.ts
+++ src/app/google/youtube-data/youtube-data.service.ts
@@ -39,7 +39,7 @@ export class YoutubeDataService {
       const videoStream = this.ytdl.download(url, {
         filter: 'audioonly',
         quality: 'highestaudio',
-        highWaterMark: 1 << 21
+        highWaterMark: 4 * 1024 * 1024,
       });
 
       const range = req.headers.range;
